f6b9d9cf1dbf95c475566d384332ddba67376496
kshitijbahul <chunks.3@gmail.com>
2024-04-04 15:44:33 +0200

    chore: Added basic code comments

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 Kshitij Bahul
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -25,6 +26,7 @@ This application uses Cobra to acchieve this.`,
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 
 import "github.com/kshitijbahul/go-cli/cmd"
 
+// This is called after all init menthod in the imported packages are run
 func main() {
 	cmd.Execute()
 }
